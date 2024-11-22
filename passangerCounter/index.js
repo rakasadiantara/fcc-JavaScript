765e62daed00f92f3dc54b46436e674e6a65f59c
rakasadiantara1 <raka@onebyone.com.sg>
2024-11-22 10:19:15 +0700

    first commit
    javascript

---
+++ passangerCounter/index.js
@@ -0,0 +1,16 @@
+let count = 0
+
+console.log(count)
+
+
+let myAge = 37;
+let humanDogRation= 8;
+
+let myDogAge= myAge * humanDogRation;
+
+let bonusPoints = 50
+ bonusPoints = bonusPoints +50
+bonusPoints= bonusPoints - 755
+
+bonusPoints= bonusPoints+ 45
+console.log(bonusPoints)
