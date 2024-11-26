cd557ef4adb361a2995067dd230aec1009aabc85
rakasadiantara1 <raka@onebyone.com.sg>
2024-11-26 14:52:24 +0700

    make a extension browser

---
+++ choromeExtension/eventListener.js
@@ -0,0 +1,5 @@
+let boxEl = document.getElementById("box")
+
+boxEl.addEventListener("click", function(){
+    console.log("halo boxy")
+})
---
+++ choromeExtension/index.js
@@ -0,0 +1,10 @@
+
+// function saveLead(){
+//     console.log("hello")
+// } 
+// ini digunakan ketika di html  berisikan onclick. bisa juga dengan cara tidak diisikan namun menggunakan addEventListener.
+
+let inputBtn = document.getElementById("input-btn")
+inputBtn.addEventListener("click", function(){
+    console.log("event listener")
+})
